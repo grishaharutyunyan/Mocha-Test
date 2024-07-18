b01e91a259aeea35b24033c764b03b06c84955f6
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-07-18 13:13:50 +0400

    mocha

---
+++ test/unit/greetings.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { mock, when, instance } from "ts-mockito";
+import { simpleGreeting, targetedGreeting } from "../../src/greetings";
+import { MyComplicatedDomainModel } from "../../src/MyComplicatedDomainModel";
+
+
+describe('simpleGreeting', function() {
+
+    it('should say hello', function() {
+        expect(simpleGreeting()).to.equal('hello');
+    });
+
+});
+
+
+describe('targetedGreeting', function() {
+
+    it(`should say hello and use the entity's name`, function() {
+        let MockedClass:MyComplicatedDomainModel = mock(MyComplicatedDomainModel);
+        when(MockedClass.name()).thenReturn('Bobby');
+
+        let bobby = instance(MockedClass);
+        console.log(bobby.name())
+
+        expect(targetedGreeting(bobby)).to.equal('Hello, Bobby!');
+    });
+
+});
